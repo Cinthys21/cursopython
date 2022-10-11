f48f67b4ea298c3acde5af6aaf251dffe9a48965
cinthya margot <canalesgallegoscinthya@gmail.com>
2022-10-11 17:22:37 -0500

    modifique el archivo variable

---
+++ Variables/variables.py
@@ -0,0 +1,10 @@
+##Una variable es un espacio para almacenar datos modificables, en la memoria de un ordenador
+##En Python una variable se define con la sintaxis:
+nombre_de_la_variable = valor_de_la_variable
+##cada variable, tiene un nombre y un valor.
+##el valor de la variable se define una sola vez.
+##cada variable tiene un tipo de dato.
+##Existe un tipo de variable que no es variable, es llamada:
+"constante"
+##la constante se utiliza para definir valores fijos que no requieran
+##etcetc
