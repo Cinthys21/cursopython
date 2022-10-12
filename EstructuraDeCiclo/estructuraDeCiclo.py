241fd70df44ad03900b3880d07c086b8256bfd91
cinthya margot <canalesgallegoscinthya@gmail.com>
2022-10-12 13:13:24 -0500

    agrego estructura de ciclo for y modifico la ubicacion de carpeta funciones

---
+++ EstructuraDeCiclo/estructuraDeCiclo.py
@@ -0,0 +1,14 @@
+## USAREMOS 2.
+## FOR Y WHILE.
+## SE USA PARA RECORRER LOS ELEMENTOS DE UN OBJETO "iterable"
+## un objeto iterable puede ser: 
+##   *una lista
+##   *una tupla
+##   *un conjunto
+##   *un diccionario.
+## for recorre los elementos del objeto y ejecuta un bloque de codigo.
+## su sintaxis es:
+for <elem> in <iterable>;
+    <tu codigo>
+## aqui ELEM es la variable en cada paso del buble.
+## el buble finaliza cuando ha recorrido todos los elementos.
